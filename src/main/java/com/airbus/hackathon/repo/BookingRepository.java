df14c229f0fe392c065241b9864b7bdee04c849c
bathlasaksham <40819945+bathlasaksham@users.noreply.github.com>
2019-12-08 14:03:39 +0530

    aa

--- src/main/java/com/airbus/hackathon/repo/BookingRepository.java
+++ src/main/java/com/airbus/hackathon/repo/BookingRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
-    @Query(value = "select b from bookings b  where b.date = :date and status in (0,1,2,5)", nativeQuery = true)
-    List<Booking> getBookedSeatsForFlightIdAndDate(@Param("date") String date);
+    @Query(value = "select * from bookings b  where b.date = :date and status in (0,1,2,5)", nativeQuery = true)
+    List<Booking> getBookedSeatsForFlightIdAndDate(@Param("date") LocalDate date);
 
 }
