0fc08f023832f47c93112a5872617a58caacf199
bathlasaksham <40819945+bathlasaksham@users.noreply.github.com>
2019-12-08 11:58:07 +0530

    abc

--- src/main/java/com/airbus/hackathon/manager/CouponManager.java
+++ src/main/java/com/airbus/hackathon/manager/CouponManager.java
@@ -5,6 +5,7 @@ import com.airbus.hackathon.repo.CouponRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 @Component
 public class CouponManager extends AbstractManager<Coupon> {
@@ -17,5 +18,8 @@ public class CouponManager extends AbstractManager<Coupon> {
         return couponRepo;
     }
 
+    public boolean isCouponValid(String coupon) {
+        return(!CollectionUtils.isEmpty(couponRepo.findByCouponId(coupon)));
+    }
 
 }
--- src/main/java/com/airbus/hackathon/manager/RouteManager.java
+++ src/main/java/com/airbus/hackathon/manager/RouteManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class RouteManager extends AbstractManager<Route> {
 
@@ -17,4 +21,13 @@ public class RouteManager extends AbstractManager<Route> {
         return routeRepo;
     }
 
+    public List<Route> getAllRoutes(LocalDate date, String source, String destination) {
+        return routeRepo.getAllRoutes(date, source, destination);
+    }
+
+    public List<List<Route>> getL2Routes(LocalDate date, String source, String destination) {
+        List<List<Route>> l2Routes = new ArrayList<>();
+        List <String> destinations = routeRepo.getAllDestinations(source, destination);
+        return l2Routes;
+    }
 }
