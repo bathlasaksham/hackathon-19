f9281bd58583890c927f78f83afdcdf73f4458c7
ashwani kumar <ashwani.kumar7@oyorooms.com>
2019-12-08 11:00:41 +0530

    added migration and entity

---
+++ src/main/java/com/airbus/hackathon/entity/Baggage.java
@@ -0,0 +1,87 @@
+package com.airbus.hackathon.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "baggages")
+public class Baggage extends AbstractEntity<Integer> implements Serializable {
+
+    private Integer bookingId;
+
+    private Float weight;
+
+    private Integer noOfItems;
+
+    private Status status;
+
+    public enum Status {
+
+        NOT_CHECKED_IN("Not Checked In"), CHECKED_IN("Checked In"), IN_TRANSIT("In Transit"), X_RAY("X Ray"),
+        CARGO_LOADING("Cargo Loading"), LOST("Lost");
+
+        private String name;
+
+        Status(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public Baggage() {
+    }
+
+    public Baggage(Integer bookingId, Float weight, Integer noOfItems, Status status) {
+        this.bookingId = bookingId;
+        this.weight = weight;
+        this.noOfItems = noOfItems;
+        this.status = status;
+    }
+
+    public Integer getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Integer bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public Integer getNoOfItems() {
+        return noOfItems;
+    }
+
+    public void setNoOfItems(Integer noOfItems) {
+        this.noOfItems = noOfItems;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Baggage{" +
+                "bookingId=" + bookingId +
+                ", weight=" + weight +
+                ", noOfItems=" + noOfItems +
+                ", status=" + status +
+                '}';
+    }
+
+}
